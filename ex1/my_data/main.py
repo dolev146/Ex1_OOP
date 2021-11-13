da66922d5e150aa19394ba987dd3c5fdb1a3a78c
dvirbo <dvir.borochov@gmail.com>
2021-11-13 23:31:34 +0200

    pre init allocate

--- ex1/my_data/main.py
+++ ex1/my_data/main.py
@@ -1,6 +1,21 @@
 import classes
 
 
+# c = classes.Building('ex1/data/Ex1_input/Ex1_Buildings/B1.json')
+# print(c.speed)
 
-c = classes.Building(r'C:\Users\Dvir\Desktop\PycharmProjects\Ex1\Ex1_OOP\ex1\data\Ex1_input\Ex1_Buildings\B1.json')
-print(c.minFloor)
+
+def elev_in_list(test_list):
+    res = len([ele for ele in test_list if isinstance(ele, dict)])
+    return str(res)
+
+
+temp = [[], [1, 2], [1, 4]]
+list1 = [[], [], [], [], [], 'text', 'text2', [], 'moreText']
+
+for x in list1:
+    if !=[]:
+
+
+
+print([x for x in list1 if x != []])
