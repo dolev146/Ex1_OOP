73d9f33f8f2ef5f42749aa761c1939975da0dbec
Dolev Dublon <62290677+dolev146@users.noreply.github.com>
2021-11-14 17:14:49 +0200

    Revert "dolev checkpoint"

---
+++ ex1/my_data/main.py
@@ -0,0 +1,21 @@
+import classes
+
+
+# c = classes.Building('ex1/data/Ex1_input/Ex1_Buildings/B1.json')
+# print(c.speed)
+
+
+def elev_in_list(test_list):
+    res = len([ele for ele in test_list if isinstance(ele, dict)])
+    return str(res)
+
+
+temp = [[], [1, 2], [1, 4]]
+list1 = [[], [], [], [], [], 'text', 'text2', [], 'moreText']
+
+for x in list1:
+    if !=[]:
+
+
+
+print([x for x in list1 if x != []])
